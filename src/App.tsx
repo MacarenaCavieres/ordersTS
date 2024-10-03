da0601a7b9376af83ee1029fc21390a6903190fa
MCavieres <maca.c.91@gmail.com>
2024-10-03 11:50:37 -0300

    se incorpora funcion para calculo de subtotal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-    const { order, addItem } = useOrder();
+    const { order, addItem, removeItem } = useOrder();
     return (
         <>
             <header className="bg-teal-400 py-5">
@@ -21,7 +22,8 @@ function App() {
                     </div>
                 </div>
                 <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-                    <OrderContents order={order} />
+                    <OrderContents order={order} removeItem={removeItem} />
+                    <OrderTotals order={order} />
                 </div>
             </main>
         </>
