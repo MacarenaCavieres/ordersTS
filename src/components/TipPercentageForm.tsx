4afd339ba2e1478299b726a0212bff07afe6c069
MCavieres <maca.c.91@gmail.com>
2024-10-03 12:09:09 -0300

    incorporacion del form para propinas

--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -1,3 +1,48 @@
-export default function TipPercentageForm() {
-    return <div>TipPercentageForm</div>;
+import { Dispatch, SetStateAction } from "react";
+
+const tipOptions = [
+    {
+        id: "tip-10",
+        value: 0.1,
+        label: "10%",
+    },
+    {
+        id: "tip-20",
+        value: 0.2,
+        label: "20%",
+    },
+    {
+        id: "tip-50",
+        value: 0.5,
+        label: "50%",
+    },
+];
+
+type TipPercentageFormProps = {
+    setTip: Dispatch<SetStateAction<number>>;
+};
+
+export default function TipPercentageForm({ setTip }: TipPercentageFormProps) {
+    return (
+        <div>
+            <h3 className="font-black text-2xl">Propina:</h3>
+            <form>
+                <div>
+                    {tipOptions.map((tip) => (
+                        <div className="flex gap-2" key={tip.id}>
+                            <label htmlFor={tip.id}>{tip.label}</label>
+
+                            <input
+                                id={tip.id}
+                                type="radio"
+                                name="tip"
+                                value={tip.value}
+                                onChange={(e) => setTip(+e.target.value)}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </form>
+        </div>
+    );
 }
